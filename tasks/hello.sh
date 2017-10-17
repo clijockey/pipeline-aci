8b47d2aef8afd525ed7cdda69a1fa479b7683336
Rob Edwards <rob.j.edwards@gmail.com>
2017-10-17 20:27:42 +0100

    initial

---
+++ tasks/hello.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+info () {
+    printf " [ \033[00;34m..\033[0m ] $1\n"
+}
+success () {
+    printf "\r\033[2K [ \033[00;32mOK\033[0m ] $1\n"
+}
+fail () {
+    printf "\r\033[2K [\033[0;31mFAIL\033[0m] $1 \n"
+    echo ''
+    exit
+}
+
+set -u
+
+info "About to do something ....."
+echo "Hello" 
+
+
